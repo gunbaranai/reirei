b98152a1ddc4e26c50ba61374f9d70a996f5950c
gunbaranai <ai.mo7g@gmail.com>
2019-08-08 07:31:29 +0700

    lupa komit rabu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-	$.ajax({
+	appendlist();
+});
+/*
+$(window).resize(function () {
+    waitForFinalEvent(function(){
+      console.log('Resize...');
+      $('main .container').empty();
+      appendlist();
+    }, 500, "some unique string");
+});
+*/
+
+function appendlist(){
+    $.ajax({
         dataType: "json",
         url: "https://recruitment.dev.rollingglory.com/mapi/gift",
         success: function (response) {
@@ -8,19 +21,42 @@ $(document).ready(function(){
             var data = response.data.gifts;
             console.log(data.length);
             var col, colInit = Math.floor($(window).width()/320);
-            var row = 1;
-            $('main .container').append('<div class="container-fluid"><div class="row" id="row'+row+'"></div></div>');
+            $('main .container').append('<div class="container-fluid"><div class="row" id="row"></div></div>');
             //console.log($(document).width());
             $.each(data, function(i, data){
-            	//console.log(data.info);
-            	if (col < 1){
-            		row++;
-            		$('main .container > div.container-fluid').append('<div class="row" id="row'+row+'"></div>');
-            		col = colInit;
-            	}
-            	$('#row'+row+'.row').append('<div class="col-md-'+(12/colInit)+'"><div class="card rounded"><div class="card-image"><img class="img-fluid" src="'+data.image+'" alt="Alternate Text" /></div>')
-            	col--;
+                var list = '';
+                var hover = '';
+                list += '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"><div class="card rounded" id="content">';
+                if (data.isNew > 0){
+                    list += '<img class="img-fluid new" src="assets/images/ic_new.png" srcset="assets/images/ic_new@2x.png 1281w" alt="New">';
+                }
+                hover += '<div class="card-img-overlay d-flex flex-column justify-content-between ';
+                if (data.stock == 0){
+                    list += '<p id="stock">Out of stock</p>';
+                    hover += 'out-of-stock">';
+                    hover += '<img class="img-fluid hover" src="assets/images/ic_batik_abu.png" srcset="assets/images/ic_batik_abu@2x.png 1281w" alt="Like">';
+                } else {
+                    hover += 'in-stock">';
+                    hover += '<img class="img-fluid hover" src="assets/images/ic_batik_ijo.png" srcset="assets/images/ic_batik_ijo@2x.png 1281w" alt="Like">';
+                }
+                list += '<a href="#" class="btn btn-primary btn-like">';
+                list += '<img class="img-fluid like" src="assets/images/ic_love – 1.png" srcset="assets/images/ic_love – 1@2x.png 1281w" alt="Like">';
+                list += '</a><div class="card-image">';
+                list += '<img class="img-fluid" src="'+data.image+'" alt="'+data.name+'" />';
+                list += '<p id="product-name">'+data.name+'</p><br/>';
+                list += '<div id="points">';
+                list += '<img class="img-fluid point" src="assets/images/ic_point-1.png" srcset="assets/images/ic_point-1@2x.png 1281w" alt="Point">';
+                list += '<p>'+data.points+' points</p></div>';
+                list += '<div id="reviews">';
+                for (var j = Math.floor(data.rating); j >= 0; j--) {
+                    list += '<img class="img-fluid point" src="assets/images/ic_bintang.png" srcset="assets/images/ic_bintang@2x.png 1281w" alt="Rating">'
+                }
+                list += '<p>'+data.num_reviews+' reviews</p><br/></div>';
+                list += hover;
+                list += '</div></div></div></div>';
+                
+                $('#row.row').append(list);
             });
         }
     });
-});
+}
